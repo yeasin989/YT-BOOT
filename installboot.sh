3e2bd5040d484e3812c3cf9dfc9f1efc1ef39f1d
Miah Mohammed Yeasin Alam <73710733+yeasin989@users.noreply.github.com>
2025-06-09 14:34:31 +0600

    Update installboot.sh

--- installboot.sh
+++ installboot.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y python3 python3-pip chromium-driver chromium-browser
-pip3 install Flask selenium
 
-git clone https://github.com/yeasin989/YT-WatchBot.git
-cd YT-WatchBot
+apt update
+apt install -y python3 python3-pip unzip wget curl
+pip3 install --upgrade pip
 
-echo "Run: python3 app.py"
+# Google Chrome & ChromeDriver
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+apt update
+apt install -y google-chrome-stable
+
+pip3 install -r requirements.txt
+
+echo "All set! Run: python3 app.py"
